a52ef4933f048fc2fe6e3e17fe60f83655effe8e
ngocphu97 <ngocphu2320@gmail.com>
2019-06-18 08:53:27 +0700

    Add decription for app

---
+++ src/app/content/content.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ContentRoutingModule } from './content-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ContentRoutingModule
+  ]
+})
+export class ContentModule { }
